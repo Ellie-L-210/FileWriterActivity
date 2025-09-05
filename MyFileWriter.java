70f5abded526b76401af75a38913c41938f0516c
Ellie Liou <eliou1@hwemail.com>
2025-09-04 22:21:12 -0700

    Add hidden and non hidden files using BufferedWriter; create .gitignore file

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,1 +1,32 @@
-hello! this is my file writer
+package FileWriterActivity;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
+
+public class MyFileWriter {
+    void makeHiddenFile(String password) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".secretcodes.txt"));
+            if (password.equals("")) {
+                bufferedWriter.write("1234veryoriginal");
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void makeHiddenFolder() {
+        try {
+            BufferedWriter bufferedWriter2 = new BufferedWriter(
+                    new FileWriter(
+                            "/Users/ellieliou/Desktop/HonorsTopics/FileWriterActivity/.classifiedinfo/donttellanyone.dat"));
+            bufferedWriter2.write("super secret phrase");
+            bufferedWriter2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- helloWorld.java
+++ helloWorld.java
@@ -1,3 +1,7 @@
+package FileWriterActivity;
+
 public class helloWorld {
-    System.out.print("hello world!");
+    public static void main(String[] args) {
+        System.out.print("hello world!");
+    }
 }
